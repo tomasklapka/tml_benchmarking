13feecd8c8dc111ac74f0ea29f24cfa84fa193e9
Tomáš Klapka <tomas@klapka.cz>
2019-02-23 09:39:43 +0100

    added compilation's comp_elapsed and comp_mem

--- tests/test_tc_souffle_compiled_prog_round.sh
+++ tests/test_tc_souffle_compiled_prog_round.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-souffle -o./tmp/tc.souffle.compiled tc.souffle
+declare -a r=`./benchmark.sh souffle -o./tmp/tc.souffle.compiled tc.souffle 2>&1 > /dev/null`
+declare -A r_a
+eval "r_a=($r)"
+>&2 echo -n "[comp_elapsed]=${r_a[elapsed]} [comp_mem]=${r_a[mem]} "
+
 ./tmp/tc.souffle.compiled -F./tmp/ -D./tmp/
--- tests/test_tc_souffle_compiled_round.sh
+++ tests/test_tc_souffle_compiled_round.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-souffle -o./tmp/comp.souffle$1 ./tmp/$1.comp.souffle
+declare -a r=`./benchmark.sh souffle -o./tmp/comp.souffle$1 ./tmp/$1.comp.souffle 2>&1 > /dev/null`
+declare -A r_a
+eval "r_a=($r)"
+>&2 echo -n "[comp_elapsed]=${r_a[elapsed]} [comp_mem]=${r_a[mem]} "
+
 ./tmp/comp.souffle$1 > ./tmp/$1.result.comp.souffle
